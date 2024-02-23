gentoo-crate-dist/railway

commit d29271802865fb0299f9c2c4663a0ba68b7f90ce

a11y(): improve screen reader support through custom accessibility labels and descriptions
The bookmark lists both get an accessibility label.

This adds labels to trips in search based on most of
its properties, and a description based on its
indicators for delays etc.

In the details view, it also adds similarly descriptive
labels to each trip segement, as well as their stopovers
and announcements.

Co-authored-by: Julian Schmidhuber <gitlab@schmiddi.anonaddy.com>
Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

src/gui/stopover_item.rs
@@ -42,6 +42,7 @@ pub mod imp {
     use gtk::DirectionType;
     use once_cell::sync::Lazy;
 
+    use crate::backend::Place;
     use crate::backend::Stopover;
     use crate::gui::alt_label::AltLabel;
     use crate::gui::utility::Utility;
@@ -53,6 +54,18 @@ pub mod imp {
         pub(super) alt_label_arrival: TemplateChild<AltLabel>,
 
         stopover: RefCell<Option<Stopover>>,
+    }
+
+    impl StopoverItem {
+        fn format_stopover_description(stop: &str, arrival: &Option<String>) -> String {
+            // Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+            let format = gettextrs::gettext("{stop} at {arrival}");
+
+            match arrival {
+                Some(arrival) => format.replace("{stop}", stop).replace("{arrival}", &arrival),
+                None => stop.to_string(),
+            }
+        }
     }
 
     #[glib::object_subclass]
@@ -74,6 +87,19 @@ pub mod imp {
     impl ObjectImpl for StopoverItem {
         fn constructed(&self) {
             self.parent_constructed();
+
+            self.obj().connect_notify_local(Some("stopover"), |stopover_item, _| {
+                let stopover = stopover_item.property::<Stopover>("stopover");
+                let stop = stopover.property::<Place>("stop");
+
+                stopover_item.update_property(&[
+                    gtk::accessible::Property::Label(&StopoverItem::format_stopover_description(
+                        &stop.name().unwrap_or_default(),
+                        &stopover.property::<Option<String>>("arrival")
+                            .or(stopover.property::<Option<String>>("planned-arrival")),
+                    ))
+                ]);
+            });
         }
 
         fn properties() -> &'static [ParamSpec] {

data/resources/ui/indicator_icons.ui
@@ -4,6 +4,14 @@
   <requires lib="libadwaita" version="1.0"/>
 
   <template class="DBIndicatorIcons" parent="GtkBox">
+    <property name="accessible-role">group</property>
+    <accessibility>
+      <relation name="described-by">img_load_factor</relation>
+      <relation name="described-by">img_late_factor</relation>
+      <relation name="described-by">img_change_platform</relation>
+      <relation name="described-by">img_unreachable</relation>
+      <relation name="described-by">img_cancelled</relation>
+    </accessibility>
     <property name="orientation">horizontal</property>
     <property name="spacing">2</property>
     <child>

data/resources/ui/journey_detail_page.ui
@@ -77,9 +77,7 @@
                           <object class="GtkLabel">
                             <property name="xalign">0</property>
                             <property name="hexpand">true</property>
-                            <accessibility>
-                              <property name="description" translatable="yes">Total Travel Time</property>
-                            </accessibility>
+                            <property name="tooltip-text" translatable="yes">Total Travel Time</property>
                             <binding name="label">
                               <lookup name="total-time">
                                 <lookup name="journey" type="DBJourneyDetailPage"/>
@@ -97,6 +95,11 @@
 
                     <child>
                       <object class="GtkBox" id="box_legs">
+                        <property name="accessible-role">list</property>
+                        <accessibility>
+                          <property name="label" translatable="yes">Trip Segments</property>
+                        </accessibility>
+
                         <property name="orientation">vertical</property>
                         <property name="spacing">12</property>
                       </object>

data/resources/ui/journey_list_item.ui
@@ -167,7 +167,7 @@
         <property name="spacing">6</property>
 
         <child>
-          <object class="DBIndicatorIcons">
+          <object class="DBIndicatorIcons" id="indicators">
             <binding name="load-factor">
               <lookup name="load-factor">
                 <lookup name="journey" type="DBJourneyListItem"/>

data/resources/ui/leg_item.ui
@@ -7,6 +7,12 @@
     <style>
       <class name="card"/>
     </style>
+    <property name="accessible-role">list-item</property>
+    <accessibility>
+      <relation name="labelled-by">segment_label</relation>
+      <relation name="details">indicators</relation>
+      <relation name="details">box_remarks</relation>
+    </accessibility>
     <property name="focusable">True</property>
 
     <child>
@@ -18,6 +24,7 @@
         <property name="margin-bottom">12</property>
         <property name="margin-start">12</property>
         <property name="margin-end">12</property>
+        <!-- TODO: make stopover-like and keyboard navigable for a11y -->
         <child>
           <object class="GtkBox">
             <property name="orientation">horizontal</property>
@@ -135,7 +142,7 @@
                     <property name="spacing">12</property> <!-- column-spacing -->
                     <!--property name="row-spacing">0</property-->
                     <child>
-                      <object class="GtkLabel">
+                      <object class="GtkLabel" id="segment_label">
                         <property name="wrap">True</property>
                         <property name="hexpand">False</property>
                         <property name="xalign">0</property>
@@ -215,7 +222,7 @@
                         <property name="spacing">6</property>
 
                         <child>
-                          <object class="DBIndicatorIcons">
+                          <object class="DBIndicatorIcons" id="indicators">
                             <property name="hexpand">true</property>
                             <property name="halign">end</property>
                             <binding name="load-factor">
@@ -249,6 +256,7 @@
                           <object class="GtkToggleButton" id="remarks_button">
                             <accessibility>
                               <property name="label" translatable="true">Show Announcements</property>
+                              <relation name="controls">box_remarks</relation>
                             </accessibility>
                             <property name="tooltip-text" translatable="true">Show Announcements</property>
 
@@ -270,6 +278,11 @@
                     <property name="vexpand">False</property>
                     <property name="child">
                       <object class="GtkBox" id="box_remarks">
+                        <property name="accessible-role">list</property>
+                        <accessibility>
+                          <property name="label" translatable="yes">Announcements</property>
+                        </accessibility>
+
                         <property name="spacing">12</property>
                         <property name="margin-start">0</property>
                         <property name="margin-end">18</property>
@@ -297,6 +310,11 @@
                 <property name="orientation">vertical</property>
                 <child>
                   <object class="GtkBox" id="box_stopovers">
+                    <property name="accessible-role">list</property>
+                    <accessibility>
+                      <property name="label" translatable="yes">Stopovers</property>
+                    </accessibility>
+
                     <property name="orientation">vertical</property>
                   </object>
                 </child>
@@ -325,6 +343,7 @@
           </object>
         </child>
 
+        <!-- TODO: make stopover-like and keyboard navigable for a11y -->
         <child>
           <object class="GtkBox">
             <property name="orientation">horizontal</property>

data/resources/ui/remark_item.ui
@@ -4,6 +4,11 @@
   <requires lib="libadwaita" version="1.0"/>
 
   <template class="DBRemarkItem" parent="GtkBox">
+    <property name="accessible-role">list-item</property>
+    <accessibility>
+      <relation name="labelled-by">announcement_label</relation>
+    </accessibility>
+
     <property name="orientation">horizontal</property>
     <property name="focusable">True</property>
     <property name="hexpand">TRUE</property>
@@ -20,7 +25,7 @@
       </object>
     </child>
     <child>
-      <object class="GtkLabel">
+      <object class="GtkLabel" id="announcement_label">
         <style>
           <class name="dim-label"/>
         </style>

data/resources/ui/search_page.ui
@@ -118,6 +118,9 @@
                 </child>
                 <child>
                   <object class="GtkListBox" id="box_journeys">
+                    <accessibility>
+                      <property name="label" translatable="yes">Bookmarked Trips</property>
+                    </accessibility>
                     <style>
                       <class name="boxed-list"/>
                     </style>
@@ -129,6 +132,9 @@
                 </child>
                 <child>
                   <object class="GtkListBox" id="box_searches">
+                    <accessibility>
+                      <property name="label" translatable="yes">Bookmarked Searches</property>
+                    </accessibility>
                     <style>
                       <class name="boxed-list"/>
                     </style>

data/resources/ui/stopover_item.ui
@@ -7,6 +7,7 @@
     <style>
       <class name="stopover-item"/>
     </style>
+    <property name="accessible-role">list-item</property>
     <property name="focusable">True</property>
 
     <property name="orientation">horizontal</property>

commit e46ac7aea3d190f767684e6181e898e6b5edd2c8

a11y(): add platforms to stopover a11y labels
Fixes: d29271802865fb0299f9c2c4663a0ba68b7f90ce

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

po/de.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: DieBahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: 2024-02-08 11:01+0000\n"
 "Last-Translator: Markus Göllnitz <camelcasenick@bewares.it>\n"
 "Language-Team: German <https://hosted.weblate.org/projects/schmiddi-on-"
@@ -248,7 +248,7 @@ msgstr "Einstellungen"
 msgid "Open Menu"
 msgstr "Hauptmenü"
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 #, fuzzy
 msgid "Swap Start and Destination"
 msgstr "Start und Ziel fehlen"
@@ -616,6 +616,11 @@ msgstr[1] "{} Zwischenhalte"
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

po/diebahn.pot
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: diebahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
@@ -236,7 +236,7 @@ msgstr ""
 msgid "Open Menu"
 msgstr ""
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 msgid "Swap Start and Destination"
 msgstr ""
 
@@ -595,6 +595,11 @@ msgstr[1] ""
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

po/es.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: diebahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: 2024-01-12 18:06+0000\n"
 "Last-Translator: gallegonovato <fran-carro@hotmail.es>\n"
 "Language-Team: Spanish <https://hosted.weblate.org/projects/schmiddi-on-"
@@ -245,7 +245,7 @@ msgstr "Ajustes"
 msgid "Open Menu"
 msgstr ""
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 msgid "Swap Start and Destination"
 msgstr ""
 
@@ -633,6 +633,11 @@ msgstr[1] "{} paradas"
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

po/fr.po
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: diebahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: 2024-02-13 08:02+0000\n"
 "Last-Translator: rene-coty <irenee.thirion@e.email>\n"
 "Language-Team: French <https://hosted.weblate.org/projects/schmiddi-on-"
@@ -249,7 +249,7 @@ msgstr "Préférences"
 msgid "Open Menu"
 msgstr "Menu principal"
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 #, fuzzy
 msgid "Swap Start and Destination"
 msgstr "Départ et destination manquants"
@@ -616,6 +616,11 @@ msgstr[1] "{} arrêts"
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

po/hr.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: diebahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: 2023-12-31 16:09+0000\n"
 "Last-Translator: Krešo Kunjas <kkunjas@gmail.com>\n"
 "Language-Team: Croatian <https://hosted.weblate.org/projects/schmiddi-on-"
@@ -246,7 +246,7 @@ msgstr "Postavke"
 msgid "Open Menu"
 msgstr ""
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 msgid "Swap Start and Destination"
 msgstr ""
 
@@ -629,6 +629,11 @@ msgstr[2] "Otvori staqnice"
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

po/it.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: diebahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: 2024-02-08 11:01+0000\n"
 "Last-Translator: boblaspugna4f1ee5d391024b4c <boblaspugna@null.net>\n"
 "Language-Team: Italian <https://hosted.weblate.org/projects/schmiddi-on-"
@@ -248,7 +248,7 @@ msgstr "Preferenze"
 msgid "Open Menu"
 msgstr "Menu Principale"
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 #, fuzzy
 msgid "Swap Start and Destination"
 msgstr "Partenza e Destinazione non indicate"
@@ -615,6 +615,11 @@ msgstr[1] "{} fermate"
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

po/nl.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: DieBahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: 2024-02-16 20:02+0000\n"
 "Last-Translator: Philip Goto <philip.goto@gmail.com>\n"
 "Language-Team: Dutch <https://hosted.weblate.org/projects/schmiddi-on-mobile/"
@@ -248,7 +248,7 @@ msgstr "Voorkeuren"
 msgid "Open Menu"
 msgstr "Hoofdmenu"
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 #, fuzzy
 msgid "Swap Start and Destination"
 msgstr "Start en bestemming ontbreken"
@@ -616,6 +616,11 @@ msgstr[1] "{} tussenstops"
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

po/pl.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: diebahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: 2024-01-22 10:01+0000\n"
 "Last-Translator: Eryk Michalak <gnu.ewm@protonmail.com>\n"
 "Language-Team: Polish <https://hosted.weblate.org/projects/schmiddi-on-"
@@ -248,7 +248,7 @@ msgstr "Preferencje"
 msgid "Open Menu"
 msgstr "Menu główne"
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 msgid "Swap Start and Destination"
 msgstr ""
 
@@ -615,6 +615,11 @@ msgstr[2] ""
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

po/ru.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: diebahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: Automatically generated\n"
 "Language-Team: none\n"
@@ -236,7 +236,7 @@ msgstr ""
 msgid "Open Menu"
 msgstr ""
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 msgid "Swap Start and Destination"
 msgstr ""
 
@@ -600,6 +600,11 @@ msgstr[2] ""
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

po/sv.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: diebahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: 2024-02-08 11:01+0000\n"
 "Last-Translator: bittin1ddc447d824349b2 <bittin@reimu.nl>\n"
 "Language-Team: Swedish <https://hosted.weblate.org/projects/schmiddi-on-"
@@ -248,7 +248,7 @@ msgstr "Inställningar"
 msgid "Open Menu"
 msgstr "Huvudmeny"
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 #, fuzzy
 msgid "Swap Start and Destination"
 msgstr "Start och destination saknas"
@@ -615,6 +615,11 @@ msgstr[1] "{} mellanlandningar"
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

po/uk.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: diebahn\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2024-02-23 19:56+0100\n"
+"POT-Creation-Date: 2024-02-23 23:29+0100\n"
 "PO-Revision-Date: 2023-12-16 12:09+0000\n"
 "Last-Translator: two <two@envs.net>\n"
 "Language-Team: Ukrainian <https://hosted.weblate.org/projects/schmiddi-on-"
@@ -243,7 +243,7 @@ msgstr ""
 msgid "Open Menu"
 msgstr ""
 
-#: data/resources/ui/station_entry.ui:16
+#: data/resources/ui/station_entry.ui:19
 msgid "Swap Start and Destination"
 msgstr ""
 
@@ -610,6 +610,11 @@ msgstr[2] "Відкрити зупинки"
 
 #. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
 #: src/gui/stopover_item.rs:62
+msgid "{stop} at {arrival} on platform {platform}"
+msgstr ""
+
+#. Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+#: src/gui/stopover_item.rs:65
 msgid "{stop} at {arrival}"
 msgstr ""
 

src/gui/stopover_item.rs
@@ -57,13 +57,26 @@ pub mod imp {
     }
 
     impl StopoverItem {
-        fn format_stopover_description(stop: &str, arrival: &Option<String>) -> String {
+        fn format_stopover_description(stop: &str, arrival: &Option<String>, platform: &Option<String>) -> String {
             // Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
-            let format = gettextrs::gettext("{stop} at {arrival}");
+            let format_full = gettextrs::gettext("{stop} at {arrival} on platform {platform}");
 
-            match arrival {
-                Some(arrival) => format.replace("{stop}", stop).replace("{arrival}", &arrival),
-                None => stop.to_string(),
+            // Translators: The formatting of the stopovers's description for screen readers. Do not translate the strings in {}.
+            let format_no_platform = gettextrs::gettext("{stop} at {arrival}");
+
+            match (arrival, platform) {
+                (Some(arrival), Some(platform)) => {
+                    format_full
+                        .replace("{stop}", stop)
+                        .replace("{arrival}", &arrival)
+                        .replace("{platform}", &platform)
+                }
+                (Some(arrival), None) => {
+                    format_no_platform
+                        .replace("{stop}", stop)
+                        .replace("{arrival}", &arrival)
+                }
+                (_, _) => stop.to_string(),
             }
         }
     }
@@ -97,6 +110,8 @@ pub mod imp {
                         &stop.name().unwrap_or_default(),
                         &stopover.property::<Option<String>>("arrival")
                             .or(stopover.property::<Option<String>>("planned-arrival")),
+                        &stopover.property::<Option<String>>("arrival-platform")
+                            .or(stopover.property::<Option<String>>("planned-arrival-platform")),
                     ))
                 ]);
             });