gentoo-crate-dist/railway

commit 42ee5e57102ad791f372ecf5b2662175a43fa920

feat(live-update-box): Add box displaying journey status and add notifications
This adds a box on top of bookmarked journeys displaying the next "event"
(i.e. start in some minutes, transition to another leg in some minutes,
...) as well as notifying about those events, and also "events" (i.e.
something is unreachable, train is later, ...).

Co-authored-by: Markus Göllnitz <camelcasenick@bewares.it>

src/backend/timer.rs
@@ -1,0 +1,114 @@
+use std::time::Duration;
+
+use gdk::{
+    glib::{self, clone, Object},
+    subclass::prelude::ObjectSubclassIsExt,
+};
+
+use super::Journey;
+
+gtk::glib::wrapper! {
+    pub struct Timer(ObjectSubclass<imp::Timer>);
+}
+
+impl Default for Timer {
+    fn default() -> Self {
+        Object::builder().build()
+    }
+}
+
+impl Timer {
+    pub fn register_background(&self, journey: Journey) {
+        let journey_id = journey.id();
+        let handle = gspawn!(clone!(
+            #[strong(rename_to = s)]
+            self,
+            async move {
+                loop {
+                    glib::timeout_future(
+                        journey
+                            .next_background_tasks_in()
+                            .to_std()
+                            .unwrap_or_default(),
+                    )
+                    .await;
+                    // Don't do background tasks if already in minutely batch.
+                    if !s.has_in_minutely(journey.id()) {
+                        journey.background_tasks();
+                    }
+                }
+            }
+        ));
+        self.imp()
+            .background_handles
+            .borrow_mut()
+            .insert(journey_id, handle);
+    }
+
+    pub fn unregister_background(&self, journey: Journey) {
+        if let Some(handle) = self
+            .imp()
+            .background_handles
+            .borrow_mut()
+            .remove(&journey.id())
+        {
+            handle.abort();
+        }
+    }
+
+    pub fn has_in_minutely(&self, journey_id: String) -> bool {
+        self.imp()
+            .minutely_handles
+            .borrow()
+            .get(&journey_id)
+            .is_some()
+    }
+
+    pub fn register_minutely(&self, journey: Journey) {
+        let journey_id = journey.id();
+        let handle = gspawn!(async move {
+            loop {
+                glib::timeout_future(Duration::from_secs(60)).await;
+                journey.background_tasks();
+            }
+        });
+        self.imp()
+            .minutely_handles
+            .borrow_mut()
+            .insert(journey_id, handle);
+    }
+
+    pub fn unregister_minutely(&self, journey: Journey) {
+        if let Some(handle) = self
+            .imp()
+            .minutely_handles
+            .borrow_mut()
+            .remove(&journey.id())
+        {
+            handle.abort();
+        }
+    }
+}
+
+mod imp {
+    use std::cell::RefCell;
+    use std::collections::HashMap;
+
+    use gdk::glib::subclass::types::ObjectSubclass;
+    use gdk::glib::{self, JoinHandle};
+    use gdk::subclass::prelude::ObjectImpl;
+
+    #[derive(Default)]
+    pub struct Timer {
+        pub(super) background_handles: RefCell<HashMap<String, JoinHandle<()>>>,
+        pub(super) minutely_handles: RefCell<HashMap<String, JoinHandle<()>>>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for Timer {
+        const NAME: &'static str = "DBTimer";
+        type Type = super::Timer;
+    }
+
+    impl ObjectImpl for Timer {}
+}

data/icons/general/bell-outline-none-symbolic.svg
@@ -1,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg height="16px" viewBox="0 0 16 16" width="16px" xmlns="http://www.w3.org/2000/svg">
+    <g fill="#222222">
+        <path d="m 8 0 c -0.828125 0 -1.5 0.671875 -1.5 1.5 c 0 0.074219 0.007812 0.148438 0.015625 0.222656 c -1.375 0.4375 -2.496094 1.445313 -3.074219 2.765625 l 1.601563 1.605469 c 0.203125 -1.480469 1.464843 -2.585938 2.957031 -2.59375 c 1.65625 0 3 1.34375 3 3 v 4.5 h -1.046875 l 2 2 h 1.546875 c 0.554688 0 1 -0.445312 1 -1 s -0.445312 -1 -1 -1 h -0.503906 v -4.40625 c 0.003906 -0.03125 0.003906 -0.0625 0.003906 -0.09375 c 0 -2.191406 -1.425781 -4.125 -3.519531 -4.777344 c 0.015625 -0.074218 0.019531 -0.148437 0.019531 -0.222656 c 0 -0.828125 -0.667969 -1.5 -1.5 -1.5 z m -4.972656 6.199219 c -0.011719 0.101562 -0.019532 0.199219 -0.027344 0.300781 v 4.5 h -0.5 c -0.554688 0 -1 0.445312 -1 1 s 0.445312 1 1 1 h 7.328125 l -2 -2 h -2.828125 v -2.828125 z m 2.972656 7.800781 c 0 0.714844 0.382812 1.375 1 1.730469 c 0.617188 0.359375 1.382812 0.359375 2 0 c 0.617188 -0.355469 1 -1.015625 1 -1.730469 z m 0 0"/>
+        <path d="m 1.53125 0.457031 l -1.0625 1.0625 l 14 14 l 1.0625 -1.0625 z m 0 0"/>
+    </g>
+</svg>

data/icons/general/bell-outline-symbolic.svg
@@ -1,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg height="16px" viewBox="0 0 16 16" width="16px" xmlns="http://www.w3.org/2000/svg">
+    <path d="m 8 0 c -0.828125 0 -1.5 0.671875 -1.5 1.5 c 0 0.074219 0.007812 0.148438 0.015625 0.222656 c -2.089844 0.652344 -3.515625 2.585938 -3.515625 4.777344 v 4.5 h -0.5 c -0.554688 0 -1 0.445312 -1 1 s 0.445312 1 1 1 h 11 c 0.554688 0 1 -0.445312 1 -1 s -0.445312 -1 -1 -1 h -0.503906 v -4.40625 c 0.003906 -0.03125 0.003906 -0.0625 0.003906 -0.09375 c 0 -2.191406 -1.425781 -4.125 -3.519531 -4.773438 c 0.015625 -0.078124 0.019531 -0.152343 0.019531 -0.226562 c 0 -0.828125 -0.667969 -1.5 -1.5 -1.5 z m 0 3.5 c 1.65625 0 3 1.34375 3 3 v 4.5 h -6 v -4.5 c 0 -1.65625 1.34375 -3 3 -3 z m -2 10.5 c 0 0.714844 0.382812 1.375 1 1.730469 c 0.617188 0.359375 1.382812 0.359375 2 0 c 0.617188 -0.355469 1 -1.015625 1 -1.730469 z m 0 0" fill="#222222"/>
+</svg>

data/resources/resources.gresource.xml.in
@@ -44,6 +44,8 @@
     <file alias="down-small-symbolic.svg">../icons/search/down-small-symbolic.svg</file>
     <file alias="bookmark-toggled-symbolic.svg">../icons/general/bookmark-toggled-symbolic.svg</file>
     <file alias="bookmark-untoggled-symbolic.svg">../icons/general/bookmark-untoggled-symbolic.svg</file>
+    <file alias="bell-outline-symbolic.svg">../icons/general/bell-outline-symbolic.svg</file>
+    <file alias="bell-outline-none-symbolic.svg">../icons/general/bell-outline-none-symbolic.svg</file>
     <file alias="delay-small-symbolic.svg">../icons/indicators/delay-small-symbolic.svg</file>
     <file alias="delay-long-small-symbolic.svg">../icons/indicators/delay-long-small-symbolic.svg</file>
     <file alias="delay-extreme-small-symbolic.svg">../icons/indicators/delay-extreme-small-symbolic.svg</file>

data/resources/ui/journey_detail_page.blp
@@ -13,6 +13,11 @@ template $DBJourneyDetailPage: Box {
   hexpand: true;
   halign: fill;
 
+  Adw.Banner {
+    revealed: bind template.show-live-box;
+    title: bind $live_banner_title(template.journey as <$DBJourney>.current-event) as <string>;
+  }
+
   ScrolledWindow {
     hscrollbar-policy: never;
     overflow: visible;
@@ -53,6 +58,7 @@ template $DBJourneyDetailPage: Box {
             }
 
             Label label_last_refreshed {
+              label: bind $format_last_refreshed(template.journey as <$DBJourney>.last-refreshed) as <string>;
               xalign: 0;
             }
           }

data/resources/ui/window.blp
@@ -24,6 +24,16 @@ template $DBWindow: Adw.ApplicationWindow {
     }
   }
 
+  Adw.Breakpoint {
+    condition ("max-width: 420sp")
+
+    setters {
+      search_view.collapsed: true;
+      result_view.collapsed: true;
+      header_bar_details.show-title: false;
+    }
+  }
+
   Adw.ToastOverlay toast_overlay {
     Adw.NavigationSplitView search_view {
       min-sidebar-width: 300;
@@ -124,11 +134,11 @@ template $DBWindow: Adw.ApplicationWindow {
               width-request: 200;
 
               [top]
-              Adw.HeaderBar {
+              Adw.HeaderBar header_bar_details {
                 $DBRefreshButton btn_reload_detail {
                   clicked => $handle_journey_reload() swapped;
                   tooltip-text: _("Refresh This Trip");
-                  refreshing: bind journey_detail_page.refresh-in-progress;
+                  refreshing: bind journey_detail_page.journey as <$DBJourney>.refresh-in-progress;
                   visible: bind $is_some(journey_detail_page.journey) as <bool>;
                 }
 
@@ -144,10 +154,25 @@ template $DBWindow: Adw.ApplicationWindow {
                   active: bind $has_journey_stored(journey_detail_page.journey) as <bool>;
                   visible: bind $is_some(journey_detail_page.journey) as <bool>;
                 }
+
+                ToggleButton btn_watch_journey {
+                  styles [
+                    "bookmark-button",
+                  ]
+
+                  clicked => $handle_watch_journey_store() swapped;
+                  icon-name: 'bell-outline-none-symbolic';
+                  notify::active => $handle_watch_button_icon() swapped;
+                  tooltip-text: _("Watch this Trip");
+                  active: bind $and(btn_watch_journey.visible, $has_journey_watched(journey_detail_page.journey) as <bool>) as <bool>;
+                  visible: bind $and($is_some(journey_detail_page.journey) as <bool>, btn_bookmark_journey.active) as <bool>;
+                }
               }
 
               content: $DBJourneyDetailPage journey_detail_page {
                 client: bind template.client;
+                timer: bind template.timer;
+                show-live-box: bind btn_watch_journey.active;
               };
             }
           };
@@ -158,6 +183,7 @@ template $DBWindow: Adw.ApplicationWindow {
 }
 
 $DBJourneysStore store_journeys {
+  timer: bind template.timer;
   add => $handle_journey_store_add() swapped;
   remove => $handle_journey_store_remove() swapped;
 }

flake.nix
@@ -59,7 +59,7 @@
             pkgs.mkShell {
               src = ./.;
               buildInputs = [];
-              nativeBuildInputs = [ pkgs-gnome.wrapGAppsHook4 pkgs.meson pkgs.gettext pkgs-gnome.glib pkgs-gnome.gtk4 pkgs-gnome.libadwaita pkgs-gnome.pkg-config pkgs-gnome.desktop-file-utils pkgs-gnome.appstream pkgs.ninja pkgs.rustc pkgs.cargo pkgs.openssl pkgs.clippy pkgs.cargo-deny pkgs.sysprof pkgs-gnome.blueprint-compiler pkgs.md4c run check prof ];
+              nativeBuildInputs = [ pkgs-gnome.wrapGAppsHook4 pkgs.meson pkgs.gettext pkgs-gnome.glib pkgs-gnome.gtk4 pkgs-gnome.libadwaita pkgs-gnome.pkg-config pkgs-gnome.desktop-file-utils pkgs-gnome.appstream pkgs.ninja pkgs.rustc pkgs.cargo pkgs.openssl pkgs.clippy pkgs.cargo-deny pkgs.sysprof pkgs-gnome.blueprint-compiler pkgs.md4c pkgs.libfaketime run check prof ];
               shellHook = ''
                 export GSETTINGS_SCHEMA_DIR=${pkgs-gnome.gtk4}/share/gsettings-schemas/${pkgs-gnome.gtk4.name}/glib-2.0/schemas/:${pkgs-gnome.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs-gnome.gsettings-desktop-schemas.name}/glib-2.0/schemas/:./build/data/
                 meson setup -Dprofile=development build

commit 4241c9c5501b4a58c08b72d622c6eda6598bdec1

fix(background-timer): terminate background loop after arrival
Without this, if the background timer ran on a journey
that was already in the past, the timer duration was
determined to be 0 resulting in an infinite loop taking
potentially a whole CPU thread.

Annoyingly, this timer is triggered before the main
window opens, if a journey was kept in the journey store
which is over, e.g. with delete-old = False or if the
journey ended shortly, within the deletion-time period.

Thus, this can even lead to Railway never starting.

Fixes: 42ee5e57102ad791f372ecf5b2662175a43fa920 ("feat(live-update-box): Add box displaying journey status and add notifications")

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

src/backend/journey.rs
@@ -114,6 +114,7 @@ impl Journey {
         let Some(duration_next_event) = current_event
             .time_of_next_action()
             .map(|t| t.with_timezone(&Local) - Local::now())
+            .filter(|duration_next_event| duration_next_event >= &Duration::zero())
         else {
             return false;
         };
@@ -129,30 +130,29 @@ impl Journey {
                 && time_since_last_refreshed > Duration::minutes(1))
     }
 
-    pub fn next_background_tasks_in(&self) -> Duration {
+    pub fn next_background_tasks_in(&self) -> Option<Duration> {
         let current_event = self.property::<BoxedAnyObject>("current-event");
         let current_event: Ref<Event> = current_event.borrow();
 
-        let Some(duration_next_event) = current_event
+        current_event
             .time_of_next_action()
             .map(|t| t.with_timezone(&Local) - Local::now())
-        else {
-            return Duration::zero();
-        };
-
-        if duration_next_event < Duration::minutes(10) {
-            Duration::minutes(1)
-        } else if duration_next_event < Duration::minutes(30) {
-            Duration::minutes(5)
-        } else if duration_next_event < Duration::hours(2)
-            || matches!(*current_event, Event::InLeg(_, _))
-        {
-            Duration::minutes(15)
-        } else if duration_next_event < Duration::days(2) {
-            Duration::hours(1)
-        } else {
-            Duration::days(1)
-        }
+            .filter(|duration_next_event| duration_next_event >= &Duration::zero())
+            .map(|duration_next_event| {
+                if duration_next_event < Duration::minutes(10) {
+                    Duration::minutes(1)
+                } else if duration_next_event < Duration::minutes(30) {
+                    Duration::minutes(5)
+                } else if duration_next_event < Duration::hours(2)
+                    || matches!(*current_event, Event::InLeg(_, _))
+                {
+                    Duration::minutes(15)
+                } else if duration_next_event < Duration::days(2) {
+                    Duration::hours(1)
+                } else {
+                    Duration::days(1)
+                }
+            })
     }
 
     pub fn background_tasks(&self) {
@@ -249,7 +249,8 @@ impl Journey {
 
         let duration_next_event = current_event
             .time_of_next_action()
-            .map(|t| t.with_timezone(&Local) - Local::now());
+            .map(|t| t.with_timezone(&Local) - Local::now())
+            .filter(|duration_next_event| duration_next_event >= &Duration::zero());
 
         let notification = match &*current_event {
             // TODO: Settings for durations

src/backend/timer.rs
@@ -25,13 +25,17 @@ impl Timer {
             self,
             async move {
                 loop {
-                    glib::timeout_future(
-                        journey
-                            .next_background_tasks_in()
-                            .to_std()
-                            .unwrap_or_default(),
-                    )
-                    .await;
+                    let Some(sleep) = journey
+                        .next_background_tasks_in()
+                        .and_then(|sleep| sleep.to_std().ok())
+                    else {
+                        s.imp()
+                            .background_handles
+                            .borrow_mut()
+                            .remove(&journey.id());
+                        break;
+                    };
+                    glib::timeout_future(sleep).await;
                     // Don't do background tasks if already in minutely batch.
                     if !s.has_in_minutely(journey.id()) {
                         journey.background_tasks();