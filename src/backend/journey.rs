gentoo-crate-dist/railway

commit 42ee5e57102ad791f372ecf5b2662175a43fa920

feat(live-update-box): Add box displaying journey status and add notifications
This adds a box on top of bookmarked journeys displaying the next "event"
(i.e. start in some minutes, transition to another leg in some minutes,
...) as well as notifying about those events, and also "events" (i.e.
something is unreachable, train is later, ...).

Co-authored-by: Markus Göllnitz <camelcasenick@bewares.it>

src/gui/journey_detail_page.rs
@@ -14,10 +14,6 @@ impl JourneyDetailPage {
         self.imp().reload(self);
     }
 
-    fn set_refresh_in_progress(&self, b: bool) {
-        self.set_property("refresh-in-progress", b)
-    }
-
     fn journey(&self) -> Option<Journey> {
         self.property("journey")
     }
@@ -29,6 +25,7 @@ pub mod imp {
 
     use chrono::Local;
     use gdk::glib::clone;
+    use gdk::glib::BoxedAnyObject;
     use gdk::glib::JoinHandle;
     use gdk::glib::MainContext;
     use gdk::glib::ParamSpec;
@@ -42,14 +39,15 @@ pub mod imp {
     use gtk::template_callbacks;
     use gtk::CompositeTemplate;
     use once_cell::sync::Lazy;
-    use rcore::RefreshJourneyOptions;
 
     use chrono::Duration;
 
     use crate::backend::Client;
+    use crate::backend::Event;
     use crate::backend::Journey;
     use crate::backend::Leg;
     use crate::backend::Place;
+    use crate::backend::Timer;
     use crate::gui::leg_item::LegItem;
     use crate::gui::transition::Transition;
     use crate::gui::utility::Utility;
@@ -63,12 +61,13 @@ pub mod imp {
         #[template_child]
         label_last_refreshed: TemplateChild<gtk::Label>,
 
-        refresh_in_progress: Cell<bool>,
+        show_live_box: Cell<bool>,
 
         journey: RefCell<Option<Journey>>,
 
         load_handle: RefCell<Option<JoinHandle<()>>>,
 
+        pub(super) timer: RefCell<Timer>,
         client: RefCell<Option<Client>>,
     }
 
@@ -87,35 +86,12 @@ pub mod imp {
                     let journey = obj.journey();
 
                     if let Some(journey) = journey {
-                        obj.set_refresh_in_progress(true);
-                        let result_journey = obj
-                            .property::<Client>("client")
-                            .refresh_journey(
-                                &journey,
-                                RefreshJourneyOptions {
-                                    stopovers: true,
-                                    language: Some(Utility::language_code()),
-                                    ..Default::default()
-                                },
-                            )
-                            .await;
-                        if let Ok(result_journey) = result_journey {
-                            obj.set_property("journey", result_journey);
-                            obj.imp().update_last_refreshed();
-                        } else {
-                            window.display_error_toast(result_journey.expect_err("A error"));
+                        if let Err(e) = journey.refresh().await {
+                            window.display_error_toast(e);
                         }
-                        obj.set_refresh_in_progress(false);
                     }
                 }
             ));
-        }
-
-        fn update_last_refreshed(&self) {
-            self.label_last_refreshed.set_label(
-                &gettextrs::gettext("Last refreshed {}")
-                    .replace("{}", &Utility::format_time_human(&Local::now().time())),
-            )
         }
     }
 
@@ -124,6 +100,18 @@ pub mod imp {
         #[template_callback(function)]
         fn format_source_destination(source: &str, destination: &str) -> String {
             format!("{source} → {destination}")
+        }
+
+        #[template_callback(function)]
+        fn format_last_refreshed(last_refreshed: Option<&str>) -> Option<String> {
+            last_refreshed.map(|s| format!("Last refreshed {}", s))
+        }
+
+        #[template_callback(function)]
+        fn live_banner_title(event: Option<BoxedAnyObject>) -> Option<String> {
+            let binding = event.as_ref().map(|e| e.borrow());
+            let event: Option<&Event> = binding.as_deref();
+            event.map(|e| e.format_at_time(Local::now()))
         }
 
         async fn setup(&self, redo: bool) {
@@ -320,8 +308,9 @@ pub mod imp {
             static PROPERTIES: Lazy<Vec<ParamSpec>> = Lazy::new(|| {
                 vec![
                     ParamSpecObject::builder::<Journey>("journey").build(),
+                    ParamSpecBoolean::builder("show-live-box").build(),
                     ParamSpecObject::builder::<Client>("client").build(),
-                    ParamSpecBoolean::builder("refresh-in-progress").build(),
+                    ParamSpecObject::builder::<Timer>("timer").build(),
                 ]
             });
             PROPERTIES.as_ref()
@@ -330,6 +319,10 @@ pub mod imp {
         fn set_property(&self, _id: usize, value: &Value, pspec: &ParamSpec) {
             match pspec.name() {
                 "journey" => {
+                    if let Some(old) = self.journey.borrow().as_ref() {
+                        self.timer.borrow().unregister_minutely(old.clone());
+                    }
+
                     let obj = value.get::<Option<Journey>>().expect(
                         "Property `journey` of `JourneyDetailPage` has to be of type `Journey`",
                     );
@@ -338,7 +331,7 @@ pub mod imp {
                     let redo = obj.as_ref().map(|j| j.id())
                         != self.journey.borrow().as_ref().map(|j| j.id());
 
-                    self.journey.replace(obj);
+                    self.journey.replace(obj.clone());
 
                     // Ensure the load is not called twice at the same time by aborting the old one if needed.
                     if let Some(handle) = self.load_handle.replace(None) {
@@ -351,14 +344,28 @@ pub mod imp {
                         glib::Priority::LOW
                     );
 
+                    if let Some(obj) = obj {
+                        if self.obj().property("show-live-box") {
+                            self.timer.borrow().register_minutely(obj);
+                        }
+                    }
+
                     self.load_handle.replace(Some(handle));
                 }
-                "refresh-in-progress" => {
+                "show-live-box" => {
                     let obj = value.get::<bool>().expect(
-                        "Property `refresh-in-progress` of `JourneyDetailPage` has to be of type `bool`",
+                        "Property `show-live-box` of `JourneyDetailPage` has to be of type `bool`",
                     );
 
-                    self.refresh_in_progress.replace(obj);
+                    if let Some(journey) = self.obj().property("journey") {
+                        if obj {
+                            self.timer.borrow().register_minutely(journey)
+                        } else {
+                            self.timer.borrow().unregister_minutely(journey)
+                        }
+                    }
+
+                    self.show_live_box.replace(obj);
                 }
                 "client" => {
                     let obj = value.get::<Option<Client>>().expect(
@@ -367,6 +374,13 @@ pub mod imp {
 
                     self.client.replace(obj);
                 }
+                "timer" => {
+                    let obj = value.get::<Timer>().expect(
+                        "Property `timer` of `JourneyDetailPage` has to be of type `timer`",
+                    );
+
+                    self.timer.replace(obj);
+                }
                 _ => unimplemented!(),
             }
         }
@@ -374,8 +388,9 @@ pub mod imp {
         fn property(&self, _id: usize, pspec: &ParamSpec) -> Value {
             match pspec.name() {
                 "journey" => self.journey.borrow().to_value(),
-                "refresh-in-progress" => self.refresh_in_progress.get().to_value(),
+                "show-live-box" => self.show_live_box.get().to_value(),
                 "client" => self.client.borrow().to_value(),
+                "timer" => self.timer.borrow().to_value(),
                 _ => unimplemented!(),
             }
         }

data/icons/general/bell-outline-none-symbolic.svg
@@ -1,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg height="16px" viewBox="0 0 16 16" width="16px" xmlns="http://www.w3.org/2000/svg">
+    <g fill="#222222">
+        <path d="m 8 0 c -0.828125 0 -1.5 0.671875 -1.5 1.5 c 0 0.074219 0.007812 0.148438 0.015625 0.222656 c -1.375 0.4375 -2.496094 1.445313 -3.074219 2.765625 l 1.601563 1.605469 c 0.203125 -1.480469 1.464843 -2.585938 2.957031 -2.59375 c 1.65625 0 3 1.34375 3 3 v 4.5 h -1.046875 l 2 2 h 1.546875 c 0.554688 0 1 -0.445312 1 -1 s -0.445312 -1 -1 -1 h -0.503906 v -4.40625 c 0.003906 -0.03125 0.003906 -0.0625 0.003906 -0.09375 c 0 -2.191406 -1.425781 -4.125 -3.519531 -4.777344 c 0.015625 -0.074218 0.019531 -0.148437 0.019531 -0.222656 c 0 -0.828125 -0.667969 -1.5 -1.5 -1.5 z m -4.972656 6.199219 c -0.011719 0.101562 -0.019532 0.199219 -0.027344 0.300781 v 4.5 h -0.5 c -0.554688 0 -1 0.445312 -1 1 s 0.445312 1 1 1 h 7.328125 l -2 -2 h -2.828125 v -2.828125 z m 2.972656 7.800781 c 0 0.714844 0.382812 1.375 1 1.730469 c 0.617188 0.359375 1.382812 0.359375 2 0 c 0.617188 -0.355469 1 -1.015625 1 -1.730469 z m 0 0"/>
+        <path d="m 1.53125 0.457031 l -1.0625 1.0625 l 14 14 l 1.0625 -1.0625 z m 0 0"/>
+    </g>
+</svg>

data/icons/general/bell-outline-symbolic.svg
@@ -1,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg height="16px" viewBox="0 0 16 16" width="16px" xmlns="http://www.w3.org/2000/svg">
+    <path d="m 8 0 c -0.828125 0 -1.5 0.671875 -1.5 1.5 c 0 0.074219 0.007812 0.148438 0.015625 0.222656 c -2.089844 0.652344 -3.515625 2.585938 -3.515625 4.777344 v 4.5 h -0.5 c -0.554688 0 -1 0.445312 -1 1 s 0.445312 1 1 1 h 11 c 0.554688 0 1 -0.445312 1 -1 s -0.445312 -1 -1 -1 h -0.503906 v -4.40625 c 0.003906 -0.03125 0.003906 -0.0625 0.003906 -0.09375 c 0 -2.191406 -1.425781 -4.125 -3.519531 -4.773438 c 0.015625 -0.078124 0.019531 -0.152343 0.019531 -0.226562 c 0 -0.828125 -0.667969 -1.5 -1.5 -1.5 z m 0 3.5 c 1.65625 0 3 1.34375 3 3 v 4.5 h -6 v -4.5 c 0 -1.65625 1.34375 -3 3 -3 z m -2 10.5 c 0 0.714844 0.382812 1.375 1 1.730469 c 0.617188 0.359375 1.382812 0.359375 2 0 c 0.617188 -0.355469 1 -1.015625 1 -1.730469 z m 0 0" fill="#222222"/>
+</svg>

data/resources/resources.gresource.xml.in
@@ -44,6 +44,8 @@
     <file alias="down-small-symbolic.svg">../icons/search/down-small-symbolic.svg</file>
     <file alias="bookmark-toggled-symbolic.svg">../icons/general/bookmark-toggled-symbolic.svg</file>
     <file alias="bookmark-untoggled-symbolic.svg">../icons/general/bookmark-untoggled-symbolic.svg</file>
+    <file alias="bell-outline-symbolic.svg">../icons/general/bell-outline-symbolic.svg</file>
+    <file alias="bell-outline-none-symbolic.svg">../icons/general/bell-outline-none-symbolic.svg</file>
     <file alias="delay-small-symbolic.svg">../icons/indicators/delay-small-symbolic.svg</file>
     <file alias="delay-long-small-symbolic.svg">../icons/indicators/delay-long-small-symbolic.svg</file>
     <file alias="delay-extreme-small-symbolic.svg">../icons/indicators/delay-extreme-small-symbolic.svg</file>

data/resources/ui/journey_detail_page.blp
@@ -13,6 +13,11 @@ template $DBJourneyDetailPage: Box {
   hexpand: true;
   halign: fill;
 
+  Adw.Banner {
+    revealed: bind template.show-live-box;
+    title: bind $live_banner_title(template.journey as <$DBJourney>.current-event) as <string>;
+  }
+
   ScrolledWindow {
     hscrollbar-policy: never;
     overflow: visible;
@@ -53,6 +58,7 @@ template $DBJourneyDetailPage: Box {
             }
 
             Label label_last_refreshed {
+              label: bind $format_last_refreshed(template.journey as <$DBJourney>.last-refreshed) as <string>;
               xalign: 0;
             }
           }

data/resources/ui/window.blp
@@ -24,6 +24,16 @@ template $DBWindow: Adw.ApplicationWindow {
     }
   }
 
+  Adw.Breakpoint {
+    condition ("max-width: 420sp")
+
+    setters {
+      search_view.collapsed: true;
+      result_view.collapsed: true;
+      header_bar_details.show-title: false;
+    }
+  }
+
   Adw.ToastOverlay toast_overlay {
     Adw.NavigationSplitView search_view {
       min-sidebar-width: 300;
@@ -124,11 +134,11 @@ template $DBWindow: Adw.ApplicationWindow {
               width-request: 200;
 
               [top]
-              Adw.HeaderBar {
+              Adw.HeaderBar header_bar_details {
                 $DBRefreshButton btn_reload_detail {
                   clicked => $handle_journey_reload() swapped;
                   tooltip-text: _("Refresh This Trip");
-                  refreshing: bind journey_detail_page.refresh-in-progress;
+                  refreshing: bind journey_detail_page.journey as <$DBJourney>.refresh-in-progress;
                   visible: bind $is_some(journey_detail_page.journey) as <bool>;
                 }
 
@@ -144,10 +154,25 @@ template $DBWindow: Adw.ApplicationWindow {
                   active: bind $has_journey_stored(journey_detail_page.journey) as <bool>;
                   visible: bind $is_some(journey_detail_page.journey) as <bool>;
                 }
+
+                ToggleButton btn_watch_journey {
+                  styles [
+                    "bookmark-button",
+                  ]
+
+                  clicked => $handle_watch_journey_store() swapped;
+                  icon-name: 'bell-outline-none-symbolic';
+                  notify::active => $handle_watch_button_icon() swapped;
+                  tooltip-text: _("Watch this Trip");
+                  active: bind $and(btn_watch_journey.visible, $has_journey_watched(journey_detail_page.journey) as <bool>) as <bool>;
+                  visible: bind $and($is_some(journey_detail_page.journey) as <bool>, btn_bookmark_journey.active) as <bool>;
+                }
               }
 
               content: $DBJourneyDetailPage journey_detail_page {
                 client: bind template.client;
+                timer: bind template.timer;
+                show-live-box: bind btn_watch_journey.active;
               };
             }
           };
@@ -158,6 +183,7 @@ template $DBWindow: Adw.ApplicationWindow {
 }
 
 $DBJourneysStore store_journeys {
+  timer: bind template.timer;
   add => $handle_journey_store_add() swapped;
   remove => $handle_journey_store_remove() swapped;
 }

flake.nix
@@ -59,7 +59,7 @@
             pkgs.mkShell {
               src = ./.;
               buildInputs = [];
-              nativeBuildInputs = [ pkgs-gnome.wrapGAppsHook4 pkgs.meson pkgs.gettext pkgs-gnome.glib pkgs-gnome.gtk4 pkgs-gnome.libadwaita pkgs-gnome.pkg-config pkgs-gnome.desktop-file-utils pkgs-gnome.appstream pkgs.ninja pkgs.rustc pkgs.cargo pkgs.openssl pkgs.clippy pkgs.cargo-deny pkgs.sysprof pkgs-gnome.blueprint-compiler pkgs.md4c run check prof ];
+              nativeBuildInputs = [ pkgs-gnome.wrapGAppsHook4 pkgs.meson pkgs.gettext pkgs-gnome.glib pkgs-gnome.gtk4 pkgs-gnome.libadwaita pkgs-gnome.pkg-config pkgs-gnome.desktop-file-utils pkgs-gnome.appstream pkgs.ninja pkgs.rustc pkgs.cargo pkgs.openssl pkgs.clippy pkgs.cargo-deny pkgs.sysprof pkgs-gnome.blueprint-compiler pkgs.md4c pkgs.libfaketime run check prof ];
               shellHook = ''
                 export GSETTINGS_SCHEMA_DIR=${pkgs-gnome.gtk4}/share/gsettings-schemas/${pkgs-gnome.gtk4.name}/glib-2.0/schemas/:${pkgs-gnome.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs-gnome.gsettings-desktop-schemas.name}/glib-2.0/schemas/:./build/data/
                 meson setup -Dprofile=development build

commit b9a2126c5394fd585bc0fc73b24f2aacaf5d1b20

fix(journey-details-page): let journeys signal updates and use that to reload the details page
Journey reloading used to live in the details page
instead of in the journeys themselves. Moving it to the
more sensible location being the journeys, however,
led to the details page not setting the new journey, but
instead the journey being updated.

This broke with the details page's assumption that a
journey update corresponds with updating the journey
property.

Because journeys are not immutable, introducing a new
signal to them signalling an update is a proper solution
for this issue.

Fixes: 42ee5e57102ad791f372ecf5b2662175a43fa920 ("feat(live-update-box): Add box displaying journey status and add notifications")

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

src/backend/journey.rs
@@ -89,6 +89,7 @@ impl Journey {
 
         if result.is_ok() {
             self.update_last_refreshed();
+            self.emit_by_name::<()>("updated", &[]);
         }
 
         result.map(|_| ())
@@ -614,6 +615,7 @@ mod imp {
         glib::{
             BoxedAnyObject, ParamSpec, ParamSpecBoolean, ParamSpecEnum, ParamSpecObject,
             ParamSpecString, Value,
+            subclass::Signal,
         },
         prelude::{ParamSpecBuilderExt, ToValue},
         subclass::prelude::{ObjectImpl, ObjectSubclass, ObjectSubclassExt},
@@ -655,6 +657,13 @@ mod imp {
     }
 
     impl ObjectImpl for Journey {
+        fn signals() -> &'static [Signal] {
+            static SIGNALS: Lazy<Vec<Signal>> = Lazy::new(|| {
+                vec![Signal::builder("updated").build()]
+            });
+            SIGNALS.as_ref()
+        }
+
         fn properties() -> &'static [ParamSpec] {
             static PROPERTIES: Lazy<Vec<ParamSpec>> = Lazy::new(|| {
                 vec![

src/gui/journey_detail_page.rs
@@ -32,6 +32,7 @@ pub mod imp {
     use gdk::glib::ParamSpecBoolean;
     use gdk::glib::ParamSpecObject;
     use gdk::glib::Value;
+    use gdk::glib::signal::SignalHandlerId;
     use glib::subclass::InitializingObject;
     use gtk::glib;
     use gtk::prelude::*;
@@ -64,6 +65,8 @@ pub mod imp {
         show_live_box: Cell<bool>,
 
         journey: RefCell<Option<Journey>>,
+
+        journey_update_signal_id: Cell<Option<SignalHandlerId>>,
 
         load_handle: RefCell<Option<JoinHandle<()>>>,
 
@@ -279,6 +282,21 @@ pub mod imp {
                 }
             }
         }
+
+        fn schedule_setup(&self, redo: bool) {
+            // Ensure the load is not called twice at the same time by aborting the old one if needed.
+            if let Some(handle) = self.load_handle.take() {
+                handle.abort();
+            }
+
+            let o = self.obj().clone();
+            let handle = gspawn!(
+                async move { o.imp().setup(redo).await },
+                glib::Priority::LOW
+            );
+
+            self.load_handle.replace(Some(handle));
+        }
     }
 
     #[glib::object_subclass]
@@ -321,6 +339,10 @@ pub mod imp {
                 "journey" => {
                     if let Some(old) = self.journey.borrow().as_ref() {
                         self.timer.borrow().unregister_minutely(old.clone());
+
+                        if let Some(id) = self.journey_update_signal_id.take() {
+                            old.disconnect(id);
+                        }
                     }
 
                     let obj = value.get::<Option<Journey>>().expect(
@@ -333,24 +355,25 @@ pub mod imp {
 
                     self.journey.replace(obj.clone());
 
-                    // Ensure the load is not called twice at the same time by aborting the old one if needed.
-                    if let Some(handle) = self.load_handle.replace(None) {
-                        handle.abort();
-                    }
-
-                    let o = self.obj().clone();
-                    let handle = gspawn!(
-                        async move { o.imp().setup(redo).await },
-                        glib::Priority::LOW
-                    );
+                    self.schedule_setup(redo);
 
                     if let Some(obj) = obj {
+                        self.journey_update_signal_id.replace(
+                            Some(obj.connect_local("updated", true, clone!(
+                                #[weak(rename_to = s)]
+                                self,
+                                #[upgrade_or_default]
+                                move |_| {
+                                    s.schedule_setup(false);
+                                    None
+                                }
+                            )))
+                        );
+
                         if self.obj().property("show-live-box") {
                             self.timer.borrow().register_minutely(obj);
                         }
                     }
-
-                    self.load_handle.replace(Some(handle));
                 }
                 "show-live-box" => {
                     let obj = value.get::<bool>().expect(